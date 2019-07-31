2570aae2b1d6adb0a07641089ddd6044939191fa
David Dyke <david.dyke@salecycle.com>
2019-07-31 14:35:33 +0100

    Updates dependencies due to security vulnerability (#1)
    * Updates dependencies due to security vulnerability
    
    * Updates gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,43 @@
-const gulp = require('gulp');
-const ts = require('gulp-typescript');
-const gulpTslint = require('gulp-tslint');
-const tslint = require('tslint');
-const del = require('del');
+const gulp = require("gulp");
+const ts = require("gulp-typescript");
+const gulpTslint = require("gulp-tslint");
+const tslint = require("tslint");
+const del = require("del");
 
-gulp.task('lint', () => {
-  const tsProject = ts.createProject('./tsconfig.json');
-  const program = tslint.Linter.createProgram('./tsconfig.json');
+function lint() {
+  const tsProject = ts.createProject("./tsconfig.json");
+  const program = tslint.Linter.createProgram("./tsconfig.json");
 
-  tsProject.src()
-    .pipe(gulpTslint({
-      formatter: 'stylish',
-      program
-    }))
-    .pipe(gulpTslint.report({
-      summarizeFailureOutput: true
-    }));
-});
+  return tsProject
+    .src()
+    .pipe(
+      gulpTslint({
+        formatter: "stylish",
+        program
+      })
+    )
+    .pipe(
+      gulpTslint.report({
+        summarizeFailureOutput: true
+      })
+    );
+}
 
-gulp.task('clean', (done) => {
-  del('.dist').then(() => {
+function clean(done) {
+  del(".dist").then(() => {
     done();
   });
-});
+}
 
-gulp.task('build', [], () => {
-  const tsProject = ts.createProject('./tsconfig.json');
-  return gulp.src('src/**/*.ts')
+function build() {
+  const tsProject = ts.createProject("./tsconfig.json");
+  return gulp
+    .src("src/**/*.ts")
     .pipe(tsProject())
-    .pipe(gulp.dest('.dist'));
-});
+    .pipe(gulp.dest(".dist"));
+}
 
-gulp.task('test', ['lint']);
-gulp.task('default', ['test']);
+exports.lint = lint;
+exports.clean = clean;
+exports.build = build;
+exports.default = lint;
